07b4bea1198763ea83911f5bc76f099bd0ebe6dc
Anggoran <anggoranm@gmail.com>
2024-08-02 14:26:28 +0700

    add: basic authentication

--- src/logics/gameAPI.js
+++ src/logics/gameAPI.js
@@ -1,4 +1,11 @@
 import {
+  getAuth,
+  updateProfile,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import {
   getFirestore,
   addDoc,
   updateDoc,
@@ -13,16 +20,31 @@ import {
 } from "firebase/firestore";
 import { firebaseApp } from "../utils/firebase";
 
+const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 const gameColl = collection(db, "games");
 const gameDoc = (gameID) => doc(gameColl, gameID);
 const roundColl = (gameID) => collection(gameDoc(gameID), "rounds");
 const roundDoc = (gameID, roundID) => doc(roundColl(gameID), roundID);
 
-export const login = () => {};
 export const subscribeAuth = () => {};
 export const getUser = () => {};
-export const logout = () => {};
+export const signup = async ({ name, email, password }) => {
+  // check vscode terminal
+  const result = await createUserWithEmailAndPassword(auth, email, password);
+  await updateProfile(result.user, { displayName: name });
+  console.log(auth.currentUser);
+};
+export const login = async ({ email, password }) => {
+  // check vscode terminal
+  const result = await signInWithEmailAndPassword(auth, email, password);
+  console.log(result);
+};
+export const logout = async () => {
+  // check vscode terminal
+  await signOut(auth);
+  console.log(auth.currentUser);
+};
 export const addGame = async (userID) => {
   // check firebase console
   await addDoc(gameColl, {
