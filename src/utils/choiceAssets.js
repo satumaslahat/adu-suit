e3b89d1291361ee0626a1d825cf2fd5c519a20e8
Anggoran <anggoranm@gmail.com>
2024-08-09 15:10:16 +0700

    add: utils

---
+++ src/utils/choiceAssets.js
@@ -0,0 +1,5 @@
+export const choiceAssets = {
+  rock: require("../../assets/images/rock.png"),
+  paper: require("../../assets/images/paper.png"),
+  scissors: require("../../assets/images/scissors.png"),
+};
---
+++ src/utils/themes.js
@@ -0,0 +1,18 @@
+export const themes = {
+  text: {
+    large: 30,
+    medium: 18,
+    small: 14,
+  },
+  colors: {
+    white: "#FFFFFF",
+    lightGrey: "#CCCCCC",
+    darkGrey: "#808080",
+    black: "#000000",
+    yellow: "#E6B319",
+    darkYellow: "#57450F",
+    lightGreen: "#D4F7E5",
+    green: "#19E67E",
+    darkGreen: "#056133",
+  },
+};
